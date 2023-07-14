6b92f1505a7dfcd1c2f8849512d05d208eccd8d9
unknown <salih.usmanovv@gmail.com>
2023-07-14 16:17:21 +0300

    contact change

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,19 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
 
-function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
-}
+const buttons = document.querySelectorAll('#btn');
 
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
-}
 
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
+buttons.forEach(btn => {
+  btn.addEventListener('click', function() {
+    buttons.forEach(button => {
+      button.classList.remove('size-active');
+      button.classList.add('size');
+    });
+    this.classList.add('size-active');
+  });
+});
+
+
+
+
 
 
